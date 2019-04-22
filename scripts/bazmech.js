f43a6253d43c155e0c82fafd434c915fbc39b87a
Barry Bell <github@bazmech.net>
2019-04-22 23:45:48 +0100

    added css and js files

---
+++ scripts/bazmech.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    document.getElementsByTagName("burger")[0].addEventListener("click", function () {
+        this.classList.toggle("change");
+        document.getElementsByTagName("menu")[0].classList.toggle("show-menu");
+    })
+
+    document.getElementsByTagName("menu")[0].addEventListener("click", function () {
+        document.getElementsByTagName("burger")[0].classList.toggle("change");
+        document.getElementsByTagName("menu")[0].classList.toggle("show-menu");
+    })
+
+    window.setTimeout(function () {
+        document.getElementsByTagName("menu")[0].classList.toggle("initial-hide");
+    }, 1000)
+})();
+
